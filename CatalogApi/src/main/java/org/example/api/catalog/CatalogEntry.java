2097f38cd3aa01ff154813af963a4a39a23a2152
Thomas Couchoud <thomas.couchoud@prestataire.sihm.fr>
2022-09-14 09:11:29 +0200

    Also implement interface

---
+++ CatalogApi/src/main/java/org/example/api/catalog/CatalogEntry.java
@@ -0,0 +1,4 @@
+package org.example.api.catalog;
+
+public interface CatalogEntry {
+}
--- PojoConverter/src/main/java/org/example/converter/AnnotatedClassBuilder.java
+++ PojoConverter/src/main/java/org/example/converter/AnnotatedClassBuilder.java
@@ -9,6 +9,7 @@ import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.matcher.NameMatcher;
 import org.example.api.catalog.Annot1;
 import org.example.api.catalog.Annot2;
+import org.example.api.catalog.CatalogEntry;
 import org.example.api.pojo.Pojo;
 import org.example.converter.definition.ClassDefinition;
 import org.example.converter.definition.Definition;
@@ -16,6 +17,7 @@ import org.example.converter.definition.FieldDefinition;
 import org.example.converter.util.JacksonUtils;
 import org.jetbrains.annotations.NotNull;
 import org.reflections.Reflections;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,6 +26,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Objects;
 import java.util.Optional;
+
 import static org.reflections.scanners.Scanners.SubTypes;
 import static org.reflections.scanners.Scanners.TypesAnnotated;
 
@@ -32,56 +35,58 @@ import static org.reflections.scanners.Scanners.TypesAnnotated;
 public class AnnotatedClassBuilder{
 	public void convert(@NotNull Path definitionPath) throws IOException{
 		try{
-			var definition = loadDefinition(definitionPath);
-			
-			new Reflections(definition.getPackageName())
-					.get(SubTypes.of(TypesAnnotated.with(Pojo.class)).asClass())
-					.forEach(klass -> {
-						var def = Optional.ofNullable(definition.getClasses().get(klass.getSimpleName()))
-								.orElseThrow(() -> new RuntimeException("Class " + klass + " has no definition"));
-						
-						annotateClass(klass, def);
-					});
+            var definition = loadDefinition(definitionPath);
+
+            new Reflections(definition.getPackageName())
+                    .get(SubTypes.of(TypesAnnotated.with(Pojo.class)).asClass())
+                    .forEach(klass -> {
+                        var def = Optional.ofNullable(definition.getClasses().get(klass.getSimpleName()))
+                                .orElseThrow(() -> new RuntimeException("Class " + klass + " has no definition"));
+
+                        annotateClass(klass, def);
+                    });
 		}
 		catch(Exception e){
-			e.printStackTrace();
-			throw e;
-		}
-	}
-	
-	@NotNull
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @NotNull
 	private Definition loadDefinition(@NotNull Path definitionPath) throws IOException{
 		try(var is = Files.newInputStream(definitionPath)){
 			return JacksonUtils.getYamlMapper().readValue(is, new TypeReference<>(){});
-		}
-	}
-	
-	@SneakyThrows
+        }
+    }
+
+    @SneakyThrows
 	private void annotateClass(@NotNull Class<?> klass, @NotNull ClassDefinition definition){
-		var byteBuddy = new ByteBuddy().redefine(klass);
+		var byteBuddy = new ByteBuddy()
+                .redefine(klass);
+        byteBuddy = byteBuddy.implement(CatalogEntry.class);
 		for(var field : klass.getDeclaredFields()){
-			var def = definition.getFields().get(field.getName());
+            var def = definition.getFields().get(field.getName());
 			if(Objects.nonNull(def)){
-				byteBuddy = byteBuddy.field(new NameMatcher<>(name -> Objects.equals(name, field.getName()))).annotateField(buildAnnotations(def));
-			}
-		}
+                byteBuddy = byteBuddy.field(new NameMatcher<>(name -> Objects.equals(name, field.getName()))).annotateField(buildAnnotations(def));
+            }
+        }
 		try(var unloaded = byteBuddy.make()){
-			var classFile = Paths.get("build/classes/java/main");
-			unloaded.saveIn(classFile.toFile());
-		}
-	}
-	
-	@NotNull
+            var classFile = Paths.get("build/classes/java/main");
+            unloaded.saveIn(classFile.toFile());
+        }
+    }
+
+    @NotNull
 	private Collection<? extends AnnotationDescription> buildAnnotations(@NotNull FieldDefinition definition){
-		var annotations = new LinkedList<AnnotationDescription>();
-		
+        var annotations = new LinkedList<AnnotationDescription>();
+
 		if(definition.isAnnot1()){
-			annotations.add(AnnotationDescription.Builder.ofType(Annot1.class).build());
-		}
+            annotations.add(AnnotationDescription.Builder.ofType(Annot1.class).build());
+        }
 		if(definition.isAnnot2()){
-			annotations.add(AnnotationDescription.Builder.ofType(Annot2.class).define("param1", "value1").build());
-		}
-		
-		return annotations;
-	}
+            annotations.add(AnnotationDescription.Builder.ofType(Annot2.class).define("param1", "value1").build());
+        }
+
+        return annotations;
+    }
 }
