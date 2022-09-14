74e0df03514a431dcef6051444268dde44d98371
Thomas Couchoud <thomas.couchoud@prestataire.sihm.fr>
2022-09-14 09:13:29 +0200

    Add annot

--- Pojo/src/main/java/org/example/pojo/FirstPojo.java
+++ Pojo/src/main/java/org/example/pojo/FirstPojo.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.api.pojo.Pojo;
+import org.jetbrains.annotations.NotNull;
 
 @Pojo
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FirstPojo{
+	@NotNull
 	private String firstField;
 	private String secondField;
 	private String thirdField;
